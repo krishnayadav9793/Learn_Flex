b0dcce017f8fc24f1b9f48d8a6b494ab2d489767
krishnayadav9793 <karanyadav21398@gmail.com>
2026-03-03 19:20:35 +0530

    neon added ✅

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ import authRoute from './routes/authRoute.js';
 import leaderRoutes from './routes/leaderBoardRoute.js';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import { connectNeon } from './util/neonConnect.js';
 
 const app=express();
+
 configDotenv();
 
 app.use(cors(
@@ -25,7 +27,7 @@ mongoose.connect(process.env.MONGO_DB_URI,{dbName:"LearnFlex"})
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-
+await connectNeon();
 
 app.get("/",(req,res,next)=>{
     res.send("Server is working ✅")
---
+++ backend/util/neonConnect.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import { neon } from '@neondatabase/serverless';
+dotenv.config();
+
+
+const sql=neon(process.env.NEON_URL)
+const connectNeon = async ()=>{
+    try {
+    const [result] = await sql`SELECT 1`;
+    console.log("PostgreSQL Connected ✅");
+  } catch (error) {
+    console.error("Database connection failed ❌", error);
+    process.exit(1);
+  }
+}
+
+export  {sql,connectNeon};
