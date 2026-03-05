c1ebcce3a49547ac692dd76e6734495ec2cb4c0b
krishnayadav9793 <karanyadav21398@gmail.com>
2026-03-05 23:16:11 +0530

    question fetching ✅

--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import quizLists from "../controllers/quizDetail.js";
+import questionsQuiz from "../controllers/quizQuestion.js";
 const quizRoute = Router();
 
 
 quizRoute.get("/list",protect,quizLists)
-
+quizRoute.get("/question/:quizId",questionsQuiz);
 
 export default quizRoute;
