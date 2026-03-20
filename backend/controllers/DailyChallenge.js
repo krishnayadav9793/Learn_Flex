9d3446764d65b00095447c23acc74ec7c1227641
Mayank Soni <202451104@iiitvadodara.ac.in>
2026-03-21 01:51:25 +0530

    adding Correct,wrong and answer

--- backend/controllers/DailyChallenge.js
+++ backend/controllers/DailyChallenge.js
@@ -5,25 +5,40 @@ export const getDailyChallenge = async (req, res) => {
   try {
 
     const result = await sql`
-      SELECT 
-        q."Ques_id" AS id,
-        q."Question_Statement" AS question,
-        q."Option_1" AS option1,
-        q."Option_2" AS option2,
-        q."Option_3" AS option3,
-        q."Option_4" AS option4,
-        q."Answer" AS correct,
-        q."difficulty",
-        dc.time_limit,
-        e.exam_name
-      FROM "DailyChallenge" dc
-      JOIN "Exam" e 
+    SELECT 
+    q."Ques_id" AS id,
+    q."Question_Statement" AS question,
+    q."Option_1" AS option1,
+    q."Option_2" AS option2,
+    q."Option_3" AS option3,
+    q."Option_4" AS option4,
+    q."Answer" AS correct,
+    q."difficulty",
+    dc.time_limit,
+    e.exam_name,
+    em.correct_marks,
+    em.wrong_marks,
+    em.unattempted_marks
+
+    FROM "DailyChallenge" dc
+
+    JOIN "Exam" e 
         ON dc.exam_id = e.exam_id
-      JOIN "DailyChallengeQuestions" dcq
+
+    JOIN "Exam_Marking" em
+        ON e.exam_id = em.exam_id
+
+    JOIN "DailyChallengeQuestions" dcq
         ON dc.challenge_id = dcq.challenge_id
-      JOIN "Questions" q
+
+    JOIN "Questions" q
         ON q."Ques_id" = dcq.ques_id
-      WHERE dc.challenge_date = CURRENT_DATE
+
+    WHERE dc.challenge_id = (
+        SELECT challenge_id
+        FROM "DailyChallenge"
+        WHERE challenge_date = CURRENT_DATE
+);
     `;
 
     if (!result.length) {
