67a548b869741f66a3990c8efe0aa6d871abecdc
krishnayadav9793 <karanyadav21398@gmail.com>
2026-03-28 00:26:31 +0530

    changing routes of weeklyQuiz

--- backend/middleware/weeklyQuiz.js
+++ backend/middleware/weeklyQuiz.js
@@ -1,10 +1,10 @@
-import { sql } from "../util/neonConnect";
+import { sql } from "../util/neonConnect.js";
 
 export async function isValidSubmission(req,res,next){
     try{
         const { data } = req.body;
         console.log(data);
-        const isAlreadyExsist = await sql`SELECT count(*) from `
+        // const isAlreadyExsist = await sql`SELECT count(*) from "Weekly_Test_Submission" where user_id=${} and test_id=${data.}`
     }catch(e){
         
     }
--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -3,10 +3,12 @@ import { protect } from "../middleware/authMiddleware.js";
 import quizLists from "../controllers/quizDetail.js";
 import questionsQuiz from "../controllers/quizQuestion.js";
 import { handleSubmition } from "../controllers/quizSumission.js";
+import { isValidSubmission } from "../middleware/weeklyQuiz.js";
 const quizRoute = Router();
 
 
 quizRoute.get("/list",protect,quizLists)
 quizRoute.get("/question/:quizId",questionsQuiz);
-quizRoute.post("/submit",protect,handleSubmition);
+quizRoute.post("/submit",[protect,isValidSubmission],handleSubmition);
+
 export default quizRoute;
