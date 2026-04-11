a4159d52a1a6ea757a02b6dda5792f0e78a20776
krishnayadav9793 <karanyadav21398@gmail.com>
2026-04-11 14:14:26 +0530

    frontend

--- backend/routes/ExamRoute.js
+++ backend/routes/ExamRoute.js
@@ -1,9 +1,9 @@
-import {Router} from  "express";
+import { Router } from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { getExams } from "../controllers/Exam.js";
 import { getSubject } from "../controllers/Subject.js";
 
-const router=Router();
+const router = Router();
 router.get("/exams", protect, getExams);
-router.get("/subjects",protect,getSubject);
+router.get("/subjects", protect, getSubject);
 export default router;
