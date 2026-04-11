dac115a7c2563f582d12f3540728430868210e56
krishnayadav9793 <karanyadav21398@gmail.com>
2026-04-11 19:40:28 +0530

    commit

--- frontend/src/components/1v1/page.jsx
+++ frontend/src/components/1v1/page.jsx
@@ -764,7 +764,7 @@ export default function CompetitionPage({ onQuizStart }) {
               </div>
               <button
                 className="btn-secondary"
-                onClick={() => navigate('/homepage')}
+                onClick={() => {navigate('/homepage'); io.emit("disconnect")}}
                 style={{ marginTop: '12px', minWidth: '180px' }}
               >
                 Cancel Search
